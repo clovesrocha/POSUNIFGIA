a17b48706dea0ef9322e25936900619fcf7c37cc
profclovesrocha <cloves.rocha@etepd.com>
2020-11-20 13:40:26 -0300

    aula #1
    Exemplo de bibliotecas Python para analisar e ver dados.

---
+++ codes/aula1.py
@@ -0,0 +1,15 @@
+#exemplo de bibliotecas Python para analisar e ver dados
+#Prof. Cloves Rocha - Pós UniFG em IA
+#Aula #1
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+ys = 200 + np.random.randn(100)
+x = [x for x in range(len(ys))]
+
+plt.plot(x, ys, '-')
+plt.fill_between(x, ys, 195, where=(ys > 195), facecolor='g', alpha=0.6)
+
+plt.title("Sample Visualization")
+plt.show()
